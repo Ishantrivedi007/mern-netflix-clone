74fbc996b3da38edf961fdf49df49505cfe6203e
Ishan.Trivedi <Ishan.Trivedi@vertisystem.local>
2025-09-26 17:49:52 +0530

    test commit

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,4 +1,5 @@
 const Footer = () => {
+  console.log("footer rendered");
   return (
     <footer className="py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800">
       <div className="flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row">
